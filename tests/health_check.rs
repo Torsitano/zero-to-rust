9c0b41f19b6aca643b8eeeaca200696cbaf70ab5
Shawn Torsitano <shawn.torsitano@gmail.com>
2022-11-04 10:50:05 -0400

    finished 3.8, updated CI, broke apart Lib

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,12 +2,15 @@
 
 use std::net::TcpListener;
 
+use sqlx::{Connection, PgConnection};
+use zero_to_rust::configuration::get_configuration;
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
 
     let port = listener.local_addr().unwrap().port();
 
-    let server = zero_to_rust::run(listener).expect("Failed to bind address");
+    let server = zero_to_rust::startup::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
 
     format!("http://127.0.0.1:{}", port)
@@ -30,6 +33,39 @@ async fn health_check_works() {
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    let client = reqwest::Client::new();
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data2() {
     let app_address = spawn_app();
     let client = reqwest::Client::new();
 
