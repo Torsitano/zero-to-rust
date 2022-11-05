dc2d6ac9d6d149e38b5df46b7d73d8b4126b5bd9
Shawn Torsitano <shawn.torsitano@gmail.com>
2022-11-05 09:27:12 -0400

    finished 3.9, adding dbpool

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 //! src/main.rs
 
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero_to_rust::configuration::get_configuration;
 use zero_to_rust::startup::run;
@@ -8,8 +9,12 @@ use zero_to_rust::startup::run;
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration");
 
+    let connection = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+
     let address = format!("127.0.0.1:{}", configuration.application_port);
 
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
