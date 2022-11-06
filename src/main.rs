65e9e567cdc987017410106155f30325fb5be322
Shawn Torsitano <shawn.torsitano@gmail.com>
2022-11-06 08:07:44 -0500

    added logger

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 //! src/main.rs
 
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero_to_rust::configuration::get_configuration;
@@ -7,6 +8,8 @@ use zero_to_rust::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("Failed to read configuration");
 
     let connection = PgPool::connect(&configuration.database.connection_string())
